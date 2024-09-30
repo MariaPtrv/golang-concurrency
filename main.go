2b1cb485b0ce288984a5092ff712f8c284e20f71
mariapetrova.pmp <mariapetrova.pmp@gmail.com>
2024-09-30 09:55:11 +0300

    One channel, signal channel

--- main.go
+++ main.go
@@ -3,56 +3,41 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	ch1 := make(chan int, 3)
-	ch1 <- 1
-	ch1 <- 2
-	ch1 <- 3
+	ch := make(chan int)
+	sig := make(chan struct{})
+	wg := sync.WaitGroup{}
 
-	ch2 := make(chan int, 3)
-	ch2 <- 4
-	ch2 <- 5
-	ch2 <- 6
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case v, ok1 := <-ch:
+				if !ok1 {
+					fmt.Println("END")
+					return
+				}
+				fmt.Println(v)
 
-	go print(ch1)
-	go print(ch2)
-	wg.Wait()
-	time.Sleep(time.Second * 2)
-	go print(merge(ch1, ch2))
-}
-
-func merge(chs ...chan int) chan int {
-	chRes := make(chan int, 6)
-	for _, ch := range chs {
-		wg.Add(1)
-		go func(ch chan int) {
-			defer wg.Done()
-			for v := range ch {
-				chRes <- v
+			case <-sig:
+				fmt.Println("DONE")
+				return
 			}
-		}(ch)
-	}
+		}
+	}()
 
-	return chRes
-}
-
-func fillCh(ch chan int) chan int {
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 5; i++ {
 		ch <- i
 	}
-	close(ch)
-	return ch
-}
+	sig <- struct{}{}
 
-func print(ch chan int) {
-	fmt.Println("chan ---")
-	for v := range ch {
-		fmt.Printf("%d ", v)
-	}
-	fmt.Println(" --- chan")
+	close(ch)
+
+	wg.Wait()
+
 }
