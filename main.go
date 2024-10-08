af8ea26d7cbd2998e7aea931717da1180d99d301
mariapetrova.pmp <mariapetrova.pmp@gmail.com>
2024-10-08 10:39:22 +0300

    in progress (merge three channels)

--- main.go
+++ main.go
@@ -1,115 +1,95 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.TODO())
-	wg := sync.WaitGroup{}
+	var wg sync.WaitGroup
+	ch1 := make(chan int, 10)
+	// ch1 <- 1
+	// ch1 <- 2
+	// ch1 <- 3
+	// close(ch1)
 
-	d1 := time.Now().Add(2 * time.Second)
-	ctx1, cancel1 := context.WithDeadline(ctx, d1)
+	ch2 := make(chan int, 10)
+	// two <- 1
+	// two <- 2
+	// two <- 3
+	// two <- 4
+	// two <- 5
+	// close(two)
 
+	ch3 := make(chan int, 10)
+	// three <- 1
+	// three <- 2
+	//close(three)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM)
+	defer close(sigCh)
+
+	wg = sync.WaitGroup{}
 	wg.Add(1)
-	go One(ctx1, &wg)
 
-	d2 := time.Now().Add(4 * time.Second)
-	ctx2, cancel2 := context.WithDeadline(ctx, d2)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case _, ok := <-ch1:
+				if !ok {
+					fmt.Println("ch1 closed")
+					return
+				}
 
-	wg.Add(1)
-	go Two(ctx2, &wg)
+			case _, ok := <-ch2:
+				if !ok {
+					fmt.Println("ch2 closed")
+					return
+				}
 
-	cancel()
-	fmt.Println("Cancel")
-	wg.Wait()
-	// time.Sleep(time.Second * 4)
+			case _, ok := <-ch3:
+				if !ok {
+					fmt.Println("ch3 closed")
+					return
+				}
 
-	cancel2()
-	fmt.Println("Cancel2")
-	cancel1()
-	fmt.Println("Cancel1")
+			case <-sigCh:
+				{
+					fmt.Println("done")
+					return
+				}
 
-}
-
-func One(ctx context.Context, wg *sync.WaitGroup) {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered. Error:\n", r)
-			wg.Done()
+			}
 		}
 	}()
-	panic("panic")
-	select {
-	case <-ctx.Done():
-		fmt.Println("Done 1")
-	}
+	go func() {
+		for {
+			time.Sleep(500 * time.Millisecond)
+			select {
+			default:
+				// i := rand.Int()
+				// ch <- i
+				i1 := rand.Int()
+				ch1 <- i1
+				i2 := rand.Int()
+				ch2 <- i2
+				i3 := rand.Int()
+				ch3 <- i3
 
-	wg.Done()
+			}
+		}
+	}()
+
+	wg.Wait()
+	close(ch1)
+	close(ch2)
+	close(ch3)
 
 }
-
-func Two(ctx context.Context, wg *sync.WaitGroup) {
-
-	select {
-	case <-ctx.Done():
-		fmt.Println("Done 2")
-	}
-
-	wg.Done()
-}
-
-// var wg sync.WaitGroup
-// ch := make(chan int)
-// sigCh := make(chan os.Signal, 1)
-// defer close(sigCh)
-// signal.Notify(sigCh, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM)
-
-// wg = sync.WaitGroup{}
-
-// wg.Add(1)
-// go func() {
-// 	defer wg.Done()
-// 	for {
-// 		select {
-// 		case v, ok1 := <-ch:
-// 			if !ok1 {
-// 				fmt.Println("END")
-// 				return
-// 			}
-// 			fmt.Println(v)
-
-// 		case <-sigCh:
-// 			fmt.Println("DONE")
-// 			return
-// 		}
-// 	}
-// }()
-
-// go func() {
-// 	for {
-// 		time.Sleep(500 * time.Millisecond)
-// 		select {
-// 		default:
-// 			i := rand.Int()
-// 			ch <- i
-// 			// i2 := rand.Int()
-// 			// ch2 <- i2
-// 			// i3 := rand.Int()
-// 			// ch3 <- i3
-// 			// i4 := rand.Int()
-// 			// ch4 <- i4
-// 		}
-// 	}
-// }()
-
-// // for i := 0; i < 5; i++ {
-// // 	ch <- i
-// // }
-// // sig <- struct{}{}
-
-// wg.Wait()
-// close(ch)
