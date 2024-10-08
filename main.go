951a59aca4c5a5df985d3ffe6db8ea7f38554778
mariapetrova.pmp <mariapetrova.pmp@gmail.com>
2024-10-08 11:08:41 +0300

    Конкурентный доступ к мапе с помощью Mutex

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type cuncurrentMap struct {
 	Map map[string]int
+	mu  sync.Mutex
 }
 
 func (c cuncurrentMap) get(key string) (int, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	v, has := c.Map[key]
 	return v, has
 }
 
 func (c cuncurrentMap) set(key string, val int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.Map[key] = val
 }
 
@@ -42,12 +48,25 @@ func main() {
 
 	wg.Add(1)
 	go func() {
+		t := time.NewTicker(time.Second * 2)
+		<-t.C
+
 		defer wg.Done()
 		for i := 1; i < 10; i++ {
-			key := strings.Join([]string{"key 2", strconv.Itoa(i)}, " ")
-			m.set(key, i)
+			key := strings.Join([]string{"key", strconv.Itoa(i)}, " ")
+			v, _ := m.get(key)
+			fmt.Println(v)
 		}
 	}()
+
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	for i := 1; i < 10; i++ {
+	// 		key := strings.Join([]string{"key 2", strconv.Itoa(i)}, " ")
+	// 		m.set(key, i)
+	// 	}
+	// }()
 
 	wg.Wait()
 }
