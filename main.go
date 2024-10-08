93ab7cd1ac70ca95a61fcff0ee7dc210297cbd54
mariapetrova.pmp <mariapetrova.pmp@gmail.com>
2024-10-08 11:44:54 +0300

    Конкурентная работа с мапой с RWMutex

--- main.go
+++ main.go
@@ -5,37 +5,38 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 )
 
 type cuncurrentMap struct {
-	Map   map[string]int
-	mutex chan int
+	Map     map[string]int
+	rwMutex sync.RWMutex
 }
 
-func (c cuncurrentMap) get(key string) (int, bool) {
-	c.mutex <- 1
+func (c *cuncurrentMap) get(key string) (int, bool) {
+	c.rwMutex.RLock()
+	defer c.rwMutex.RUnlock()
 	v, has := c.Map[key]
-	<-c.mutex
+
 	return v, has
 }
 
-func (c cuncurrentMap) set(key string, val int) {
-	c.mutex <- 1
+func (c *cuncurrentMap) set(key string, val int) {
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
 	c.Map[key] = val
-	<-c.mutex
+
 }
 
 func main() {
 	var wg sync.WaitGroup
 
-	m := cuncurrentMap{
-		Map:   make(map[string]int),
-		mutex: make(chan int, 1),
+	m := &cuncurrentMap{
+		Map: make(map[string]int),
 	}
 
 	m.set("one", 1)
 	v, _ := m.get("one")
+	//_, _ = m.get("one")
 	fmt.Println(v)
 
 	wg.Add(1)
@@ -52,8 +53,8 @@ func main() {
 
 	wg.Add(1)
 	go func() {
-		t := time.NewTicker(time.Second * 2)
-		<-t.C
+		// t := time.NewTicker(time.Second * 2)
+		// <-t.C
 
 		defer wg.Done()
 		for i := 1; i < 10; i++ {
